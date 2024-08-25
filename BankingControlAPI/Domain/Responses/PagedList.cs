a1273f94e07fc6a12396b483ad58bea0d0c5e7cd
Ahmed Abdel-Razek <ahmed_master23@hotmail.com>
2024-08-25 18:38:50 +0300

    Add PaginationParameters and PagedList classes to handle page data

---
+++ BankingControlAPI/Domain/Responses/PagedList.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankingControlAPI.Domain.Responses
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; init; }
+        public int TotalPages { get; init; }
+        public int PageSize { get; init; }
+        public int TotalCount { get; init; }
+
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            AddRange(items);
+        }
+
+        public static async Task<PagedList<T>> ToPagedListAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
---
+++ BankingControlAPI/Domain/System/PaginationParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace BankingControlAPI.Domain.System
+{
+    public abstract class PaginationParameters
+    {
+        public int? PageNumber { get; init; }
+        public int? PageSize { get; init; }
+
+        public PaginationParameters()
+        {
+            PageNumber ??= 1;
+            PageSize ??= 20;
+
+            if (PageSize > 50)
+            {
+                PageSize = 50;
+            }
+        }
+    }
+}
